9c9f810abba80f126c60bc78838c5d9d58c2c338
Saachi Roye <sroye@skillgigs.com>
2019-03-04 16:27:18 -0600

    Added elasticsearch and MSSQL

--- NodeJSApps/search/server.js
+++ NodeJSApps/search/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const request = require('request');
 
 // Constants
 const PORT = 8081;
@@ -10,6 +11,12 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/search', (req, res) => {
   res.send('Hello world this is search service\n');
+});
+app.get('/search/es', (req, res) => {
+  var response = res;
+  request('http://elasticsearch:9200', (err, res, body) => {
+    response.send(res);
+  });
 });
 
 app.listen(PORT, HOST);
